8d339550a67ef70feda108eb70901d3e48be4a6f
Himanshu Yadav <himanshu07319@gmail.com>
2024-06-14 18:15:10 -0500

    add test function for fourWay cache

---
+++ test_fourWayCache.cc
@@ -0,0 +1,38 @@
+#include "src/fourWayCache/fourWayCache.hh"
+
+int main(){
+    FourWayCache cache;
+
+    cache.setAddr(0x23, READ);
+    cache.printAddr();
+    cache.run();
+    cache.incrementCycleTime();
+
+    cache.setAddr(0x1023, READ);
+    cache.printAddr();
+    cache.run();
+    cache.incrementCycleTime();
+
+    cache.setAddr(0x2023,READ);
+    cache.printAddr();
+    cache.run();
+    cache.incrementCycleTime();
+
+    cache.setAddr(0x3023,READ);
+    cache.printAddr();
+    cache.run();
+    cache.incrementCycleTime();
+
+    cache.setAddr(0x4023,READ);
+    cache.printAddr();
+    cache.run();
+    cache.incrementCycleTime();
+
+
+
+    cache.printStats();
+    cache.printValidCache();
+
+    return 0;
+}
+
